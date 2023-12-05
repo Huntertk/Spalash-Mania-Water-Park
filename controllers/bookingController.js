1a27dfcfef0c540d43923b30c60804fa60cca2b0
Huntertk <taufik.khan65@gmail.com>
2023-12-05 14:10:09 +0530

    Payment Redirect URL Set

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -39,8 +39,8 @@ export const createBooking = async (req, res) => {
                 },
             ],
             mode: 'payment',
-            success_url: `https://skyworlds-theme-pak.onrender.com/${responseClientUrl}`,
-            cancel_url: 'https://skyworlds-theme-pak.onrender.com/',
+            success_url: `https://splash-mania-waterpark.onrender.com/${responseClientUrl}`,
+            cancel_url: 'https://splash-mania-waterpark.onrender.com/',
             payment_intent_data: {
                 setup_future_usage: 'off_session',
                 description: 'Booking payment',
